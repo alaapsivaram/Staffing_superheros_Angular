9b33b6598cdf647ce19110c7107a9a91a714bd9e
Alaap Sivaram <alaap.sivaram@gmail.com>
2018-07-19 00:11:18 -0700

    Checkpoint 3 - List of Heros
    Used ngFor adn ngIf and added styling for the list elements. ALso added (click) event handler and dynamically maintaining the selected class for the clicked hero

--- angular-tour-of-heros/src/app/heros/heros.component.ts
+++ angular-tour-of-heros/src/app/heros/heros.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import {HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heros',
   templateUrl: './heros.component.html',
   styleUrls: ['./heros.component.css']
 })
+
 export class HerosComponent implements OnInit {
-  hero: Hero = {
-  		id: 1,
-		name: 'Windstorm'  
-  };
-  
+  heroes = HEROES
+  selectedHero: Hero;
   constructor() { }
 
+ onSelect( hero: Hero) {
+ 	this.selectedHero = hero;
+ }
+	
   ngOnInit() {
   }
 
