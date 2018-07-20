207c3f2e818ef57d9d0df651c938dd6d6d6afc1c
Alaap Sivaram <alaap.sivaram@gmail.com>
2018-07-20 00:28:33 -0700

    Hero service added to abstract data retrival from Component

--- angular-tour-of-heros/src/app/heros/heros.component.ts
+++ angular-tour-of-heros/src/app/heros/heros.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import {HEROES} from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heros',
@@ -9,15 +9,21 @@ import {HEROES} from '../mock-heroes';
 })
 
 export class HerosComponent implements OnInit {
-  heroes = HEROES
+  heroes: Hero[];
   selectedHero: Hero;
-  constructor() { }
+  
+  constructor(private heroService: HeroService) { }
 
  onSelect( hero: Hero) {
  	this.selectedHero = hero;
  }
+ 
+ getHeroes(): void {
+ 	this.heroes = this.heroService.getHeroes();
+ }
 	
   ngOnInit() {
+  	this.getHeroes();
   }
 
 }
